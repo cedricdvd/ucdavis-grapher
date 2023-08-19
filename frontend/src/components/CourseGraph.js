207283c056a6b0b4b958e5d19332cac0d21a5f5f
dvyno <cedricjdvd@gmail.com>
2023-08-19 15:34:26 -0700

    Fix graph so nodes are centered

--- frontend/src/components/CourseGraph.js
+++ frontend/src/components/CourseGraph.js
@@ -7,51 +7,60 @@ import './styles/CourseGraph.css';
 
 
 function CourseGraph({ root_course, prerequisite_arr }) {
-    const [ width, setWidth ] = useState(0);
-    const [ xShift, setXShift ] = useState(0);
     const ref = useRef(null);
+    const [ nodes, setNodes ] = useState([]);
+    const [ edges, setEdges ] = useState([]);
+    const nodeTypes = useMemo(
+        () => ({ rootNode : RootNode, prerequisiteNode : PrerequisiteNode }),
+        []
+    );
 
     useEffect(() => {
-        setWidth(ref.current.clientWidth);
-        setXShift(Math.floor(prerequisite_arr.length / 2) - (prerequisite_arr.length % 2 === 0 ? 0.5 : 0));
-    }, [prerequisite_arr]);
-
-    const nodeTypes = useMemo(() => ({ rootNode : RootNode, prerequisiteNode: PrerequisiteNode }), []);
-
-    let initialNodes = [{
-        id: '0',
-        data: {label : root_course.code },
-        position: {x: Math.floor(width / 2), y: 50},
-        type: 'rootNode',
-    }];
-
-    let initialEdges = []
-
-    let group_num = 0;
-    prerequisite_arr.forEach((group_arr) => {
-        initialNodes.push(
-            {
-                id: `${group_num + 1}`,
-                data: {group : group_arr },
-                position: {x: Math.floor(width / 2) + (200 * (group_num - xShift)), y: 150},
-                type: 'prerequisiteNode'
-            }
+        const width = ref.current.clientWidth;
+        const xShift = Math.floor(prerequisite_arr.length / 2) - (prerequisite_arr.length % 2 === 0 ? 0.5 : 0);
+        const prerequisite_nodes = prerequisite_arr.map(
+            (group_arr, group_idx) => (
+                {
+                    id: `${group_idx + 1}`,
+                    data: { group : group_arr },
+                    position: {x : Math.floor(width / 2) + 200 * (group_idx - xShift), y: 150 },
+                    type: 'prerequisiteNode'
+                }
+            )
         );
-        initialEdges.push(
-            {id: `0-${group_num + 1}$`, source: '0', target: `${group_num + 1}`}
-        )
-        group_num++;
-    })
+        const prerequisite_edges = prerequisite_arr.map(
+            (group_arr, group_idx) => (
+                {
+                    id: `0-${group_idx + 1}`,
+                    source: '0',
+                    target: `${group_idx + 1}`,
+                }
+            )
+        );
 
+        setNodes([
+            {
+                id: '0',
+                data: { label : root_course.code },
+                position: { x : Math.floor(width / 2), y : 50 },
+                type: 'rootNode'
+            },
+            ...prerequisite_nodes
+        ]);
+
+        setEdges(prerequisite_edges);
+    });
 
     return (
         <div ref={ref} className="prerequisite-map">
-            <ReactFlow nodes={initialNodes} edges={initialEdges} nodeTypes={nodeTypes} >
-                <Background />
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                nodeTypes={nodeTypes}
+            >
                 <Controls />
             </ReactFlow>
         </div>
-
     );
 };
 
