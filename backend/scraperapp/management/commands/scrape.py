6cae608c560f88bedcd888b0d652552ba0152c4d
dvyno <cedricjdvd@gmail.com>
2023-10-05 11:52:15 -0700

    Benchmark scraper with timer

--- backend/scraperapp/management/commands/scrape.py
+++ backend/scraperapp/management/commands/scrape.py
@@ -1,8 +1,13 @@
 from django.core.management.base import BaseCommand
 from scraperapp.scraper import scrape_data
+import time
+
 
 class Command(BaseCommand):
     help = 'Scrapes the course catalog and stores the data in the database'
-    
+
     def handle(self, *args, **kwargs):
+        start = time.perf_counter()
         scrape_data()
+        end = time.perf_counter()
+        print(f'Finished parsing in {end - start:0.4f} seconds')
